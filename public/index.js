f0126314cd7bdf4e8482f16d98c78a830e5c7b73
barenike <m-s-ka@mail.ru>
2022-06-08 16:44:46 +0500

    Amendments 40

--- public/index.js
+++ public/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './src/main/resources/static/src/App';
-import reportWebVitals from './src/main/resources/static/src/reportWebVitals';
-import {AuthProvider} from "./src/main/resources/static/src/context/AuthProvider";
+import App from '../src/main/resources/static/src/App';
+import reportWebVitals from '../src/main/resources/static/src/reportWebVitals';
+import {AuthProvider} from "../src/main/resources/static/src/context/AuthProvider";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {Provider} from "react-redux";
-import {store,persistor} from './src/main/resources/static/src/redux/store'
+import {store,persistor} from '../src/main/resources/static/src/redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
