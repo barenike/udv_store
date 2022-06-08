f6983c5c6fcfa04bec8943e5a3a449fa802f4b47
barenike <m-s-ka@mail.ru>
2022-06-08 15:18:17 +0500

    Amendments 37

---
+++ src/main/js/src/pages/AdminProduct.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from "react";
+import BalanceIncrease from "../components/BalanceIncrease";
+import AdmNavbar from "../components/AdmNavbar";
+export const AdminProduct = () => {
+    return (
+        <Fragment>
+            <AdmNavbar/>
+        </Fragment>
+    )
+
+}
---
+++ src/main/js/src/redux/cartRedux.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        products: [],
+        quantity: 0,
+        total: 0,
+    },
+    reducers: {
+        addProduct: (state, action) => {
+            state.quantity += action.payload.quantity;
+            state.products.push(action.payload);
+            state.total += action.payload.price * action.payload.quantity;
+        },
+        removeProduct: (state, action) => {
+            state.quantity -=action.payload.quantity;
+            // alert(action.payload.id);
+            // let bug =state.products.map(product => product.quantity);
+            // alert(bug);
+            // alert(action.payload.quantity)
+            state.total -= action.payload.price * action.payload.quantity;
+            // state.products[0].quantity=state.products[0].quantity-action.payload.quantity;
+            let ass;
+             (state.quantity===0)?state.products.pop(action.payload.products):alert('Удаляем не все продукты');
+            state.products.map((product, index) => {
+              console.log('Дошло', product.id === action.payload.id);
+                console.log('Количество',product.quantity)
+                console.log('Удаляем', action.payload.quantity)
+                ass =  (product.id === action.payload.id)? index: null;
+            }
+            )
+            state.products[ass].quantity=state.products[ass].quantity-action.payload.quantity;
+        },
+    },
+});
+
+export const { addProduct } = cartSlice.actions;
+export const { removeProduct } = cartSlice.actions;
+export default cartSlice.reducer;
